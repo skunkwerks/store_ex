573466e13bf38e8460db27757687aea7ded692fa
Dave Cottlehuber <dch@jsonified.com>
2014-02-05 19:52:36 +0100

    store: add exunit test for init/0

--- test/store_test.exs
+++ test/store_test.exs
@@ -1,7 +1,12 @@
 defmodule StoreTest do
+
+  @empty_store "Store.put(:foo, :baa, [])"
+  @small_store "Store.put(:foo, :baa, @empty_store)"
+  @large_store "Store.put(:few, :bar, @small_store)"
+
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "init/0 returns OK" do
+    assert Store.init == :ok
   end
 end
