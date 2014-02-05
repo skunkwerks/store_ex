245b7813f7bb9da737a60f1f3412236608ce5ca3
Dave Cottlehuber <dch@jsonified.com>
2014-02-05 19:15:43 +0100

    store: add init/0

--- lib/store.ex
+++ lib/store.ex
@@ -1,2 +1,19 @@
 defmodule Store do
+  @moduledoc """
+  Welcome to Store!
+
+  Store is a minimal Elixir application designed to help you get started
+  with behaviours and hot code upgrades. The commits are carefully
+  arranged in small easy-to-understand steps.
+  """
+  
+  @doc """
+  Initialises a new Store service.
+  Takes no parameters.
+  Returns :ok or {:error, reason} if unable to continue.
+  Should be used on module load only.
+  """
+  def init() do
+    :ok
+  end
 end
