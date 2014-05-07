c69a7c5b956971ff14d952371d9791630071c2a0
Dave Cottlehuber <dch@skunkwerks.at>
2014-05-07 09:59:47 +0200

    mix: update to latest Elixir and add hex.pm support

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule Store.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :store,
-      version: "0.0.1",
-      elixir: "~> 0.12.3",
+      version: "0.0.2",
+      elixir: "~> 0.13.0",
       deps: deps ]
   end
 
